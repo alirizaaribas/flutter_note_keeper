438eae0fe7b0a6bedd2fd6e5c5f1004c1b7b9771
alirizaaribas <alirizaaribas@gmail.com>
2019-08-10 00:12:22 +0300

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Not defteri',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Not Defteri'),
     );
   }
 }
